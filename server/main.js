5792b658da18e305c4053b4fadbe0310a48c4201
Anita Lipsky <anita@purplebugs.com>
2022-06-06 18:58:38 +1000

    tested authentication to my elastic cloud

--- server/main.js
+++ server/main.js
@@ -1,7 +1,57 @@
-const dotenv = require("dotenv");
+"use strict";
 
-dotenv.config();
+const dotenv = require("dotenv").config();
 
-console.log(process.env.USER_ID);
+const { Client } = require("@elastic/elasticsearch");
+const client = new Client({
+  cloud: {
+    id: process.env.ELASTIC_CLOUD_ID,
+  },
+  auth: {
+    username: process.env.ELASTIC_AUTH_USERNAME,
+    password: process.env.ELASTIC_AUTH_PASSWORD,
+  },
+});
 
-console.log("anita");
+async function run() {
+  // Let's start by indexing some data
+  await client.index({
+    index: "game-of-thrones",
+    document: {
+      character: "Ned Stark",
+      quote: "Winter is coming.",
+    },
+  });
+
+  await client.index({
+    index: "game-of-thrones",
+    document: {
+      character: "Daenerys Targaryen",
+      quote: "I am the blood of the dragon.",
+    },
+  });
+
+  await client.index({
+    index: "game-of-thrones",
+    document: {
+      character: "Tyrion Lannister",
+      quote: "A mind needs books like a sword needs a whetstone.",
+    },
+  });
+
+  // here we are forcing an index refresh, otherwise we will not
+  // get any result in the consequent search
+  await client.indices.refresh({ index: "game-of-thrones" });
+
+  // Let's search!
+  const result = await client.search({
+    index: "game-of-thrones",
+    query: {
+      match: { quote: "winter" },
+    },
+  });
+
+  console.log(result.hits.hits);
+}
+
+run().catch(console.log);
